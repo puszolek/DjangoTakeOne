f2b597c20b5d5cbfae947714cf255a1c6fb4dff4
paula <paula@wandlee.com>
2017-11-18 11:32:50 +0100

    index.html changes

--- main_app/views.py
+++ main_app/views.py
@@ -3,13 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 class Person:
-    def __init__(self, name, age, location, food):
+    def __init__(self, name, age, location, food, img_url):
         self.name = name
         self.age = age
         self.location = location
         self.food = food
+        self.img_url = img_url
 
-people = [Person('Paula', 23, 'Warsaw', 'pizza'), Person('Mateusz', 23, 'Warsaw', 'spaghetti')]
+people = [Person('Paula', 23, 'Warsaw', 'pizza', 'static/images/girl.png'), Person('Mateusz', 23, 'Warsaw', 'spaghetti', 'static/images/boy.png')]
 
 def index(request):
 
